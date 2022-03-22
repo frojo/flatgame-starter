27ee46a6c38c73a257ab71a2251a48136875dcd1
fran <franmorojo@gmail.com>
2022-03-21 22:38:08 -0400

    its winnie now

---
+++ flatgame-starter/Assets/RestartR.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartR : MonoBehaviour
+{
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.R)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
